2f9b7f6f60aa9a483d64e00584fcc3367f3a596d
Cornel Gurin <you@example.comcornelgurin@gmail.com>
2020-10-22 10:44:35 +0200

    Initial commit

---
+++ src/Cornel/Task_01.java
@@ -0,0 +1,8 @@
+package Cornel;
+
+public class Task_01 {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Nik/Task_01.java
@@ -0,0 +1,10 @@
+package Nik;
+
+public class Task_01 {
+    public class Task_01 {
+        public static void main(String[] args){
+
+            System.out.println("Hello World");
+        }
+    }
+}
